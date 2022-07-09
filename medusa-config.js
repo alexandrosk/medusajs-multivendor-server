685d232ec8189dd73574b44d696e0880333a6384
alexandros.koukis alexandros.koukis@vaimo.com <alexandros.koukis@vaimo.com>
2022-07-09 14:37:20 +0300

    init commit for dev

--- medusa-config.js
+++ medusa-config.js
@@ -34,10 +34,10 @@ module.exports = {
   projectConfig: {
     // redis_url: REDIS_URL,
     // For more production-like environment install PostgresQL
-    // database_url: DATABASE_URL,
-    // database_type: "postgres",
+    database_url: "postgres://postgres:postgres@replug-admin-postgres-1:5432/medusa-docker",
+    database_type: "postgres",
     database_database: "./medusa-db.sql",
-    database_type: "sqlite",
+//    database_type: "sqlite",
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
   },
